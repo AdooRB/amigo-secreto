cbffae08a7692ffe1c0304234e195a680cffd49b
Aldo Flores <sarduts@gmail.com>
2025-01-29 23:37:38 -0600

    crea el esqueleto de funciones esenciales

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//ids: input(amigo), ul(listaAmigo), ul(resultado)
+
+
+//Agregar texto a un elemento HTML
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+//Función para botón "añadir"
+function agregarAmigo(){
+    return;
+}
+
+//Función para botón "sortear amigo"
+function sortearAmigo(){
+    return;
+}
