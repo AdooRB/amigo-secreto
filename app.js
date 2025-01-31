8626e686a24004e0c684d398bc90511fba110075
Aldo Flores <sarduts@gmail.com>
2025-01-30 22:12:02 -0600

    Actualiza de la funcion 'Agregar Amigo'

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 //ids: input(amigo), ul(listaAmigo), ul(resultado)
+let listaDeNombres = [];
 
 
 //Agregar texto a un elemento HTML
@@ -11,9 +12,13 @@ function asignarTextoElemento(elemento, texto){
 
 //Función para botón "añadir"
 function agregarAmigo(){
-    return;
-}
+    let entrada = document.querySelector('#amigo');
+    
+    entrada.value == ""? alert("Por favor, inserte un nombre"): listaDeNombres.push(entrada.value);
+    console.log(`Lista de Nombres: ${listaDeNombres}`);
+    entrada.value = "";
 
+}
 //Función para botón "sortear amigo"
 function sortearAmigo(){
     return;
